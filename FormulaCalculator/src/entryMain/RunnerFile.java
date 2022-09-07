cb662ffec5af8b04b473c7086b19b1f5553a7204
kunalbiswas92 <jsmith.u1@gmail.com>
2022-09-07 22:04:25 +0530

    2nd stable code

--- FormulaCalculator/src/entryMain/RunnerFile.java
+++ FormulaCalculator/src/entryMain/RunnerFile.java
@@ -26,6 +26,29 @@ public class RunnerFile extends JFrame {
 	private JScrollPane scrollPane;
 	private static final DecimalFormat df = new DecimalFormat("0.00");
 	private JButton btnNewButton_1;
+	long FirstRowLow ;
+	long FirstRowCurrent ;
+	long FirstRowHigh ;
+
+	// Last row values
+	long LastRowLow ;
+	long LastRowCurrent ;
+	long LastRowHigh ;
+
+	// 0 degree row values
+	long ZeroDegreeLow ;
+	long ZeroDegreeCurrent ;
+	long ZeroDegreeHigh ;
+
+	// 360 degree row values
+	long ThreeSixtyDegreeLow ;
+	long ThreeSixtyDegreeCurrent;
+	long ThreeSixtyDegreeHigh ;
+
+	// Constant difference per column
+	int DifferenceLow ;
+	int DifferenceCurrent ;
+	int DifferenceHigh;
 	/**
 	 * Launch the application.
 	 */
@@ -60,6 +83,7 @@ public class RunnerFile extends JFrame {
 		contentPane.add(lblNewLabel);
 
 		textField = new JTextField();
+		
 
 		textField.setBounds(104, 10, 133, 20);
 		contentPane.add(textField);
@@ -85,34 +109,34 @@ public class RunnerFile extends JFrame {
 				System.out.println("integer part of input number : " + integerPart);
 
 				// First row values
-				long FirstRowLow = integerPart - 1;
-				long FirstRowCurrent = integerPart;
-				long FirstRowHigh = integerPart + 1;
+				 FirstRowLow = integerPart - 1;
+				 FirstRowCurrent = integerPart;
+				 FirstRowHigh = integerPart + 1;
 
 				// Last row values
-				long LastRowLow = integerPart;
-				long LastRowCurrent = integerPart + 1;
-				long LastRowHigh = integerPart + 2;
+				 LastRowLow = integerPart;
+				 LastRowCurrent = integerPart + 1;
+				 LastRowHigh = integerPart + 2;
 
 				// 0 degree row values
-				long ZeroDegreeLow = (long) Math.pow(FirstRowLow, 2);
+				 ZeroDegreeLow = (long) Math.pow(FirstRowLow, 2);
 				System.out.println("ZeroDegreeLow: " + ZeroDegreeLow);
-				long ZeroDegreeCurrent = (long) Math.pow(FirstRowCurrent, 2);
+				 ZeroDegreeCurrent = (long) Math.pow(FirstRowCurrent, 2);
 				System.out.println("ZeroDegreeCurrent: " + ZeroDegreeCurrent);
-				long ZeroDegreeHigh = (long) Math.pow(FirstRowHigh, 2);
+				 ZeroDegreeHigh = (long) Math.pow(FirstRowHigh, 2);
 				System.out.println("ZeroDegreeHigh: " + ZeroDegreeHigh);
 
 				// 360 degree row values
-				long ThreeSixtyDegreeLow = (long) Math.pow(LastRowLow, 2);
-				long ThreeSixtyDegreeCurrent = (long) Math.pow(LastRowCurrent, 2);
-				long ThreeSixtyDegreeHigh = (long) Math.pow(LastRowHigh, 2);
+				 ThreeSixtyDegreeLow = (long) Math.pow(LastRowLow, 2);
+				 ThreeSixtyDegreeCurrent = (long) Math.pow(LastRowCurrent, 2);
+				 ThreeSixtyDegreeHigh = (long) Math.pow(LastRowHigh, 2);
 
 				// Constant difference per column
-				int DifferenceLow = (int) (ThreeSixtyDegreeLow - ZeroDegreeLow);
+				 DifferenceLow = (int) (ThreeSixtyDegreeLow - ZeroDegreeLow);
 				System.out.println("DifferenceLow: " + DifferenceLow);
-				int DifferenceCurrent = (int) (ThreeSixtyDegreeCurrent - ZeroDegreeCurrent);
+				 DifferenceCurrent = (int) (ThreeSixtyDegreeCurrent - ZeroDegreeCurrent);
 				System.out.println("DifferenceCurrent: " + DifferenceCurrent);
-				int DifferenceHigh = (int) (ThreeSixtyDegreeHigh - ZeroDegreeHigh);
+				 DifferenceHigh = (int) (ThreeSixtyDegreeHigh - ZeroDegreeHigh);
 				System.out.println("DifferenceHigh: " + DifferenceHigh);
 
 				System.out.println("*************************************");
@@ -241,6 +265,176 @@ public class RunnerFile extends JFrame {
 						{ null, LastRowLow, LastRowCurrent, LastRowHigh, null }, },
 						new String[] { "VALUE1", "LOW", "CURRENT", "HIGH", "VALUE2" }));
 
+			}
+		});
+		
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Userinput = textField.getText();
+				int integerPart = Calculations.sqrtIntegerExtract(Userinput);
+
+				// integer part of input number
+				System.out.println("integer part of input number : " + integerPart);
+
+				// First row values
+				 FirstRowLow = integerPart - 1;
+				 FirstRowCurrent = integerPart;
+				 FirstRowHigh = integerPart + 1;
+
+				// Last row values
+				 LastRowLow = integerPart;
+				 LastRowCurrent = integerPart + 1;
+				 LastRowHigh = integerPart + 2;
+
+				// 0 degree row values
+				 ZeroDegreeLow = (long) Math.pow(FirstRowLow, 2);
+				System.out.println("ZeroDegreeLow: " + ZeroDegreeLow);
+				 ZeroDegreeCurrent = (long) Math.pow(FirstRowCurrent, 2);
+				System.out.println("ZeroDegreeCurrent: " + ZeroDegreeCurrent);
+				 ZeroDegreeHigh = (long) Math.pow(FirstRowHigh, 2);
+				System.out.println("ZeroDegreeHigh: " + ZeroDegreeHigh);
+
+				// 360 degree row values
+				 ThreeSixtyDegreeLow = (long) Math.pow(LastRowLow, 2);
+				 ThreeSixtyDegreeCurrent = (long) Math.pow(LastRowCurrent, 2);
+				 ThreeSixtyDegreeHigh = (long) Math.pow(LastRowHigh, 2);
+
+				// Constant difference per column
+				 DifferenceLow = (int) (ThreeSixtyDegreeLow - ZeroDegreeLow);
+				System.out.println("DifferenceLow: " + DifferenceLow);
+				 DifferenceCurrent = (int) (ThreeSixtyDegreeCurrent - ZeroDegreeCurrent);
+				System.out.println("DifferenceCurrent: " + DifferenceCurrent);
+				 DifferenceHigh = (int) (ThreeSixtyDegreeHigh - ZeroDegreeHigh);
+				System.out.println("DifferenceHigh: " + DifferenceHigh);
+
+				System.out.println("*************************************");
+				System.out.println("************ LOW column *************");
+				System.out.println("*************************************");
+				String ThirtyDegreeLow = df.format( ZeroDegreeLow +(double)DifferenceLow / 12);// 1764 + (85/12)
+				System.out.println(ThirtyDegreeLow);
+				String FortyFiveDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 8);
+				System.out.println(FortyFiveDegreeLow);
+				String SixtyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 6);
+				System.out.println(SixtyDegreeLow);
+				String NinetyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 4);
+				System.out.println(NinetyDegreeLow);
+				String OneTwentyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 3);
+				System.out.println(OneTwentyDegreeLow);
+				String OneThirtyFiveDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 2.666);
+				System.out.println(OneThirtyFiveDegreeLow);
+				String OneFiftyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 2.4);
+				System.out.println(OneFiftyDegreeLow);
+				String OneEightyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 2);
+				System.out.println(OneEightyDegreeLow);
+				String TwoTenDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.71428);
+				System.out.println(TwoTenDegreeLow);
+				String TwoTwoFiveDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.6);
+				System.out.println(TwoTwoFiveDegreeLow);
+				String TwoFortyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.5);
+				System.out.println(TwoFortyDegreeLow);
+				String TwoSeventyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.333);
+				System.out.println(TwoSeventyDegreeLow);
+				String ThreeHundredDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.2);
+				System.out.println(ThreeHundredDegreeLow);
+				String ThreeHundredFifteenDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.1428);
+				System.out.println(ThreeHundredFifteenDegreeLow);
+				String ThreeHundredThirtyDegreeLow =  df.format( ZeroDegreeLow + (double)DifferenceLow / 1.090909);
+				System.out.println(ThreeHundredThirtyDegreeLow);
+
+				
+		
+				System.out.println("*************************************");
+				System.out.println("************ Current column *************");
+				System.out.println("*************************************");
+				String ThirtyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 12);
+				System.out.println(ThirtyDegreeCurrent);
+				String FortyFiveDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 8);
+				System.out.println(FortyFiveDegreeCurrent);
+				String SixtyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 6);
+				System.out.println(SixtyDegreeCurrent);
+				String NinetyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 4);
+				System.out.println(NinetyDegreeCurrent);
+				String OneTwentyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 3);
+				System.out.println(OneTwentyDegreeCurrent);
+				String OneThirtyFiveDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 2.666);
+				System.out.println(OneThirtyFiveDegreeCurrent);
+				String OneFiftyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 2.4);
+				System.out.println(OneFiftyDegreeCurrent);
+				String OneEightyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 2);
+				System.out.println(OneEightyDegreeCurrent);
+				String TwoTenDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.71428);
+				System.out.println(TwoTenDegreeCurrent);
+				String TwoTwoFiveDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.6);
+				System.out.println(TwoTwoFiveDegreeCurrent);
+				String TwoFortyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.5);
+				System.out.println(TwoFortyDegreeCurrent);
+				String TwoSeventyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.333);
+				System.out.println(TwoSeventyDegreeCurrent);
+				String ThreeHundredDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.2);
+				System.out.println(ThreeHundredDegreeCurrent);
+				String ThreeHundredFifteenDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.1428);
+				System.out.println(ThreeHundredFifteenDegreeCurrent);
+				String ThreeHundredThirtyDegreeCurrent = df.format(ZeroDegreeCurrent + (double)DifferenceCurrent / 1.090909);
+				System.out.println(ThreeHundredThirtyDegreeCurrent);
+				
+				System.out.println("*************************************");
+				System.out.println("************ High column *************");
+				System.out.println("*************************************");
+				String ThirtyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 12);
+				System.out.println(ThirtyDegreeHigh);
+				String FortyFiveDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 8);
+				System.out.println(FortyFiveDegreeHigh);
+				String SixtyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 6);
+				System.out.println(SixtyDegreeHigh);
+				String NinetyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 4);
+				System.out.println(NinetyDegreeHigh);
+				String OneTwentyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 3);
+				System.out.println(OneTwentyDegreeHigh);
+				String OneThirtyFiveDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 2.666);
+				System.out.println(OneThirtyFiveDegreeHigh);
+				String OneFiftyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 2.4);
+				System.out.println(OneFiftyDegreeHigh);
+				String OneEightyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 2);
+				System.out.println(OneEightyDegreeHigh);
+				String TwoTenDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 1.71428);
+				System.out.println(TwoTenDegreeHigh);
+				String TwoTwoFiveDegreeHigh = df.format(ZeroDegreeHigh +(double)DifferenceHigh / 1.6);
+				System.out.println(TwoTwoFiveDegreeHigh);
+				String TwoFortyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 1.5);
+				System.out.println(TwoFortyDegreeHigh);
+				String TwoSeventyDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 1.333);
+				System.out.println(TwoSeventyDegreeHigh);
+				String ThreeHundredDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 1.2);
+				System.out.println(ThreeHundredDegreeHigh);
+				String ThreeHundredFifteenDegreeHigh = df.format(ZeroDegreeHigh + (double)DifferenceHigh / 1.1428);
+				System.out.println(ThreeHundredFifteenDegreeHigh);
+				String ThreeHundredThirtyDegreeHigh = df.format(ZeroDegreeHigh + (double) DifferenceHigh / 1.090909);
+				System.out.println(ThreeHundredThirtyDegreeHigh);
+
+				table.setModel(new DefaultTableModel(new Object[][] { 
+						{ "", FirstRowLow, FirstRowCurrent, FirstRowHigh, null },
+						{ "0", ZeroDegreeLow, ZeroDegreeCurrent, ZeroDegreeHigh, "360" }, 
+						{ "30", ThirtyDegreeLow, ThirtyDegreeCurrent, ThirtyDegreeHigh, "330" },
+						{ "45", FortyFiveDegreeLow, FortyFiveDegreeCurrent, FortyFiveDegreeHigh, "315" }, 
+						{ "60", SixtyDegreeLow, SixtyDegreeCurrent, SixtyDegreeHigh, "300" },
+						{ "90", NinetyDegreeLow, NinetyDegreeCurrent, NinetyDegreeHigh, "270" }, 
+						{ "120", OneTwentyDegreeLow, OneTwentyDegreeCurrent, OneTwentyDegreeHigh, "240" },
+						{ "135", OneThirtyFiveDegreeLow, OneThirtyFiveDegreeCurrent, OneThirtyFiveDegreeHigh, "225" }, 
+						{ "150", OneFiftyDegreeLow, OneFiftyDegreeCurrent, OneFiftyDegreeHigh, "210" },
+						{ "180", OneEightyDegreeLow, OneEightyDegreeCurrent, OneEightyDegreeHigh, "180" }, 
+						{ "210", TwoTenDegreeLow, TwoTenDegreeCurrent, TwoTenDegreeHigh, "150" },
+						{ "225", TwoTwoFiveDegreeLow, TwoTwoFiveDegreeCurrent, TwoTwoFiveDegreeHigh, "135" }, 
+						{ "240", TwoFortyDegreeLow, TwoFortyDegreeCurrent, TwoFortyDegreeHigh, "120" },
+						{ "270", TwoSeventyDegreeLow, TwoSeventyDegreeCurrent, TwoSeventyDegreeHigh, "90" }, 
+						{ "300", ThreeHundredDegreeLow, ThreeHundredDegreeCurrent, ThreeHundredDegreeHigh, "60" },
+						{ "315", ThreeHundredFifteenDegreeLow, ThreeHundredFifteenDegreeCurrent, ThreeHundredFifteenDegreeHigh, "45" }, 
+						{ "330", ThreeHundredThirtyDegreeLow, ThreeHundredThirtyDegreeCurrent, ThreeHundredThirtyDegreeHigh, "30" },
+						{ "360", ThreeSixtyDegreeLow, ThreeSixtyDegreeCurrent, ThreeSixtyDegreeHigh, "0" }, 
+						{ null, LastRowLow, LastRowCurrent, LastRowHigh, null }, },
+						new String[] { "VALUE1", "LOW", "CURRENT", "HIGH", "VALUE2" }));
+
+			
+				
 			}
 		});
 		btnNewButton.setBounds(268, 9, 89, 23);
